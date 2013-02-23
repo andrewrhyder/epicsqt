dabbc023b40e11b51a7abbfc0fa888c6eba77794
andrewstarritt <andrewstarritt@bb1e2deb-b14e-4109-9c4b-d361c50373b5>
2013-02-23 06:35:51 +0000

    and update framework version to 2.2.34

--- framework/include/QEFrameworkVersion.h
+++ framework/include/QEFrameworkVersion.h
@@ -30,16 +30,17 @@
 #include <QString>
 #include <QEPluginLibrary_global.h>
 
-#define STRINGIFY2(s)     #s
-#define STRINGIFY(s)      STRINGIFY2(s)
+#define STRINGIFY2(s)         #s
+#define STRINGIFY(s)          STRINGIFY2(s)
 
-#define QE_VERSION_MAJOR     2
-#define QE_VERSION_MINOR     2
-#define QE_VERSION_RELEASE   33
+#define QE_VERSION_MAJOR      2
+#define QE_VERSION_MINOR      2
+#define QE_VERSION_RELEASE    34
 
-#define QE_VERSION_STRING    STRINGIFY(QE_VERSION_MAJOR) "." \
-                             STRINGIFY(QE_VERSION_MINOR) "." \
-                             STRINGIFY(QE_VERSION_RELEASE)
+#define QE_VERSION_STRING     STRINGIFY(QE_VERSION_MAJOR) "." \
+                              STRINGIFY(QE_VERSION_MINOR) "." \
+                              STRINGIFY(QE_VERSION_RELEASE)
+
 #define QE_VERSION_DATE_TIME  __DATE__ " " __TIME__
 
 // Provides values used to build shared library.
