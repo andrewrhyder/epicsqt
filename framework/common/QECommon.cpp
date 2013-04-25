c2f37d0b9f1e1288ee7595999cd79c99ff3fbb43
andrewstarritt <andrewstarritt@bb1e2deb-b14e-4109-9c4b-d361c50373b5>
2013-04-25 01:08:58 +0000

    create common utilities class add initial functions

---
+++ framework/common/QECommon.cpp
@@ -0,0 +1,83 @@
+/*  QECommon.cpp
+ *
+ *  This file is part of the EPICS QT Framework, initially developed at the Australian Synchrotron.
+ *
+ *  The EPICS QT Framework is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The EPICS QT Framework is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with the EPICS QT Framework.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Copyright (c) 2013
+ *
+ *  Author:
+ *    Andrew Starritt
+ *  Contact details:
+ *    andrew.starritt@synchrotron.org.au
+ *
+ */
+
+#include "QECommon.h"
+
+
+//------------------------------------------------------------------------------
+//
+QColor QEUtilities::fontColour (const QColor & backgroundColour)
+{
+   QColor result;
+   int r, g, b, a;
+   int wc;
+   bool isDark = false;
+
+   // Split colour into components.
+   //
+   backgroundColour.getRgb (&r, &g, &b, &a);
+
+   // Counting the perceptive luminance - human eye favors green color...
+   // Form weighted component wc. Weights based on algorithm:
+   // http://stackoverflow.com/questions/1855884/determine-font-color-based-on-background-color
+   //
+   wc = ((299 * r) + (587 * g) + (114 * b)) / 1000;   // 299 + 587 + 114 = 1000
+
+   // Dark or bright background colour ?
+   //
+   isDark = (wc < 112);
+   if (isDark) {
+      result = QColor (255, 255, 255, 255);    // white font
+   } else {
+      result = QColor (0, 0, 0, 255);          // black font
+   }
+
+   return result;
+}
+
+//------------------------------------------------------------------------------
+//
+QString QEUtilities::colourToStyle (const QColor backgroundColour) {
+   QString result;
+   QColor foregroundColour;
+   int br, bg, bb, ba;
+   int fr, fg, fb, fa;
+
+   // Choose high contrast font/foreground colour.
+   //
+   foregroundColour = QEUtilities::fontColour (backgroundColour);
+
+   // Split colours into components and aggragate into a style sheet.
+   //
+   backgroundColour.getRgb (&br, &bg, &bb, &ba);
+   foregroundColour.getRgb (&fr, &fg, &fb, &fa);
+
+   result.sprintf ("QWidget { background-color: #%02x%02x%02x; color: #%02x%02x%02x; }",
+                   br, bg, bb, fr, fg, fb );
+   return result;
+}
+
+// end
