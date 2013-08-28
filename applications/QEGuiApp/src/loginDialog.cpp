a4aeaee5cd14c2b2470cfb54546eebe518ec7f21
andrewstarritt <andrewstarritt@bb1e2deb-b14e-4109-9c4b-d361c50373b5>
2013-08-28 04:09:53 +0000

    Modify loginDialog to explicitly set size, and inherit from QEDialog. Also QEGui::login API change - pass in form sothat dialog can be correctly centered

--- applications/QEGuiApp/src/loginDialog.cpp
+++ applications/QEGuiApp/src/loginDialog.cpp
@@ -1,4 +1,5 @@
-/*
+/*  loginDialog.cpp
+ *
  *  This file is part of the EPICS QT Framework, initially developed at the Australian Synchrotron.
  *
  *  The EPICS QT Framework is free software: you can redistribute it and/or modify
@@ -53,4 +54,14 @@ loginDialog::loginDialog()
 
     // Set the dialog title
     setWindowTitle( "Change User Level" );
+
+    // Explicity set the desired size. If we don't and then apply scaling, the sensible
+    // default sizing "goes out the window"..
+    //
+    QRect g = geometry();
+    g.setWidth( 200 );
+    g.setHeight( 280 );
+    setGeometry( g );
 }
+
+// end
