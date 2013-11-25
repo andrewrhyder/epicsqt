3d4546db638caefcd76739217b2b3e825d4212a5
rhyder <rhyder@bb1e2deb-b14e-4109-9c4b-d361c50373b5>
2013-11-25 21:28:26 +0000

    In QEPlotter, change name of structure from PushButtonSpecifications to PBSpecs to avoid QString destructor being called without constructor ever being called with qt 4.7.4. Strange but true. Perhaps a MOC problem?

--- framework/widgets/QEPlotter/QEPlotterToolBar.cpp
+++ framework/widgets/QEPlotter/QEPlotterToolBar.cpp
@@ -34,7 +34,7 @@
 #include "QEPlotterToolBar.h"
 #include <QEPlotterMenu.h>
 
-struct PushButtonSpecifications {
+struct PBSpecs {
    int gap;
    int width;
    bool isIcon;                         // when false is caption
@@ -47,7 +47,7 @@ struct PushButtonSpecifications {
 #define GAP                8          // group gap
 #define ICW                26         // icon width
 
-static const struct PushButtonSpecifications buttonSpecs [] = {
+static const struct PBSpecs buttonSpecs [] = {
    { 0,   ICW, true,  QString ("go_back.png"),       QEPlotterNames::PLOTTER_PREV,               QString ("Previous state")     },
    { 0,   ICW, true,  QString ("go_fwd.png"),        QEPlotterNames::PLOTTER_NEXT,               QString ("Next state")         },
 
