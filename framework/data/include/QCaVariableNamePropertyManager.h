af8a97104705f58cc9d99844cdfb875c4ad4b0ff
andrewstarritt <andrewstarritt@bb1e2deb-b14e-4109-9c4b-d361c50373b5>
2014-02-17 11:06:15 +0000

    qualify getVariableNameProperty/getSubstitutionsProperty functions as const

--- framework/data/include/QCaVariableNamePropertyManager.h
+++ framework/data/include/QCaVariableNamePropertyManager.h
@@ -1,4 +1,5 @@
-/*
+/*  QCaVariableNamePropertyManager.h
+ *
  *  This file is part of the EPICS QT Framework, initially developed at the Australian Synchrotron.
  *
  *  The EPICS QT Framework is free software: you can redistribute it and/or modify
@@ -14,7 +15,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with the EPICS QT Framework.  If not, see <http://www.gnu.org/licenses/>.
  *
- *  Copyright (c) 2009, 2010
+ *  Copyright (c) 2009, 2010, 2014
  *
  *  Author:
  *    Andrew Rhyder
@@ -36,10 +37,10 @@ class QCaVariableNamePropertyManager : public QTimer {
   public:
     QCaVariableNamePropertyManager();
 
-    QString getVariableNameProperty();
+    QString getVariableNameProperty() const;
     void setVariableNameProperty( QString variableNamePropertyIn );
 
-    QString getSubstitutionsProperty();
+    QString getSubstitutionsProperty() const;
     void setSubstitutionsProperty( QString substitutionsPropertyIn );
 
     void setVariableIndex( unsigned int variableIndexIn );
