6e45e45d1f7eeab5996c4045488d711fb0db3acc
rhyder <rhyder@bb1e2deb-b14e-4109-9c4b-d361c50373b5>
2014-04-16 01:34:07 +0000

    Revert to 2.7.1 as release script is meant to commit the change to a new version

--- trunk/framework/common/QEFrameworkVersion.h
+++ trunk/framework/common/QEFrameworkVersion.h
@@ -44,7 +44,7 @@
 // (Also, after tagging QE_VERSION_STAGE should be set to QE_VERSION_STAGE_DEVELOPMENT)
 //
 #define QE_VERSION_MAJOR      2     // Public releases that include interface or paradigm changes that may require changes to the way the framework is used.
-#define QE_VERSION_MINOR      8     // Public releases containing new functionality (may include fixes as well)
+#define QE_VERSION_MINOR      7     // Public releases containing new functionality (may include fixes as well)
 #define QE_VERSION_RELEASE    1     // Public releases containing fixes only.
 
 // During tagging the version stage should be set to production
@@ -52,7 +52,7 @@
 //
 #define QE_VERSION_STAGE_DEVELOPMENT "Development"
 #define QE_VERSION_STAGE_PRODUCTION  "Production"
-#define QE_VERSION_STAGE             QE_VERSION_STAGE_PRODUCTION
+#define QE_VERSION_STAGE             QE_VERSION_STAGE_DEVELOPMENT
 
 #define QE_VERSION_STRING     STRINGIFY(QE_VERSION_MAJOR) "." \
                               STRINGIFY(QE_VERSION_MINOR) "." \
