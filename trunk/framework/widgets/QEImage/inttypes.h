48425eb391f18b55e3bb4568ddee99a870ac949e
rhyder <rhyder@bb1e2deb-b14e-4109-9c4b-d361c50373b5>
2014-04-22 13:50:20 +0000

    Comments only

--- trunk/framework/widgets/QEImage/inttypes.h
+++ trunk/framework/widgets/QEImage/inttypes.h
@@ -1,3 +1,31 @@
+/*
+ *  This file is part of the EPICS QT Framework, initially developed at the Australian Synchrotron.
+ *
+ *  The EPICS QT Framework is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The EPICS QT Framework is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with the EPICS QT Framework.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Copyright (c) 2014
+ *
+ *  Author:
+ *    Andrew Rhyder
+ *  Contact details:
+ *    andrew.rhyder@synchrotron.org.au
+ */
+
+/*
+  This file contains definitions missing when including ffmpeg components and building under Visual Studio
+ */
+
 typedef signed char  int8_t;
 typedef signed short int16_t;
 typedef signed int   int32_t;
