17c4268b1b858d93d423c833c40a31d85e8b925f
rhyder <rhyder@bb1e2deb-b14e-4109-9c4b-d361c50373b5>
2014-07-28 12:52:22 +0000

    Avoid 'passing NULL to non-pointer argument' warning.

--- framework/widgets/QERadioGroup/QERadioGroup.cpp
+++ framework/widgets/QERadioGroup/QERadioGroup.cpp
@@ -244,7 +244,7 @@ void QERadioGroup::valueUpdate (const long &value,
    this->currentIndex = value;
 
    if (this->valueToIndex.containsF (value)) {
-      selectedIndex = this->valueToIndex.valueF (value, NULL);
+      selectedIndex = this->valueToIndex.valueF (value, 0);
       this->internalWidget->setValue (selectedIndex);
    } else {
       // We haven't mapped this value - use hidden selection.
